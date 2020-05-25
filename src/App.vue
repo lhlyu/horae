8f3652430c5f5acb8f093c6f4b8cbe828e9cd395
lhlyu <lhl103512@163.com>
2020-05-26 00:34:56 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,47 @@
 <template>
-    <div id="app">
-        <router-view></router-view>
-    </div>
+  <div id="app">
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </div>
 </template>
 
 <script>
+export default {
+  name: "app"
+}
 
-    export default {
-        name: 'App',
-        components:{
-
-        },
-        methods:{
-            init(){
-            }
-        }
-    }
 </script>
 
-<!--<style lang="scss">-->
-<!--    @import './style/index.scss';-->
+<style lang="scss">
+body {
+	margin: 0px;
+	padding: 0px;
+	font-family: Microsoft YaHei, Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,  SimSun, sans-serif;
+	font-size: 14px;
+	-webkit-font-smoothing: antialiased;
+}
 
-<!--</style>-->
+#app {
+	position: absolute;
+	top: 0px;
+	bottom: 0px;
+	width: 100%;
+}
+a{
+  color: #56a9ff;
+}
+.fade-enter-active,
+.fade-leave-active {
+	transition: all .2s ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+	opacity: 0;
+}
+#nprogress .bar {
+  height: 3px !important;
+  background: #56a9ff !important; //自定义颜色
+}
+</style>
