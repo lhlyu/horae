e23231306c1d2e743695fade6181cb89fc6b4494
lhlyu <2438211370@qq.com>
2020-06-04 16:27:35 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,6 @@ import {getRoutes,getDynamicRoutes} from "@/router/dynamic.js"
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-    mode: "history",
     // base: process.env.BASE_URL,
     routes
 })
