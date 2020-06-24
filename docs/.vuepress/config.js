8566d0228197e58e848222d8937ee53cf26ed51a
lhlyu <lhl103512@163.com>
2020-06-25 02:03:53 +0800

    docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,6 @@
-const isProduction = process.env.NODE_ENV === 'production'
-
 module.exports = {
   title: 'Horae',
   description: '管理系统',
   dest: './public/docs',
-  publicPath: isProduction ? 'https://cdn.jsdelivr.net/gh/lhlyu/horae@gh-pages/docs/' : '/'
+  publicPath: 'https://cdn.jsdelivr.net/gh/lhlyu/horae@gh-pages/docs/'
 }
